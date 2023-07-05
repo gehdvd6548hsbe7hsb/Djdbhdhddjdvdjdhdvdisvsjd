7ed1b12ea7675059a0afdf7549f34d1898cdda14
gehdvd6548hsbe7hsb <133530142+gehdvd6548hsbe7hsb@users.noreply.github.com>
2023-07-05 14:35:18 +0300

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -202,7 +202,8 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
-        pattern: "iswa",
+        pattern: "تخمين",
+        alias :['iswa','خمن'],
         category: "search",
         desc: "Searches in given rage about given number.",
         use: '9112345678xx',
@@ -210,8 +211,8 @@ cmd({
     },
     async(Void, citel, text) => {
         var inputnumber = text.split(" ")[0]
-        if (!inputnumber.includes('x')) return citel.reply('You did not add x\nExample: iswa 9196285162xx')
-        citel.reply(`Searching for WhatsApp account in given range...`)
+        if (!inputnumber.includes('x')) return citel.reply('مثال: .تخمين +192925866xx')
+        citel.reply(`اصبر...`)
 
         function countInstances(string, word) {
             return string.split(word).length - 1;
@@ -227,9 +228,9 @@ cmd({
         } else if (random_length == 3) {
             randomxx = 1000
         }
-        var text = `*--『 List of Whatsapp Numbers 』--*\n\n`
-        var nobio = `\n*Bio:* || \nHey there! I am using WhatsApp.\n`
-        var nowhatsapp = `\n*Numbers with no WhatsApp account within provided range.*\n`
+        var text = `*--『 تخمين برعاية رايزل 』--*\n\n`
+        var nobio = `\n*بايو:* || \nمرحباً انا استخدم واتساب.\n`
+        var nowhatsapp = `\n*ارقام لم تدخل في واتساب.*\n`
         for (let i = 0; i < randomxx; i++) {
             var nu = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
             var status1 = nu[Math.floor(Math.random() * nu.length)]
@@ -257,7 +258,7 @@ cmd({
                 if (anu1 == '401' || anu1.status.length == 0) {
                     nobio += `wa.me/${anu[0].jid.split("@")[0]}\n`
                 } else {
-                    text += `🧐 *Number:* wa.me/${anu[0].jid.split("@")[0]}\n ✨*Bio :* ${anu1.status}\n🍁*Last update :* ${moment(anu1.setAt).tz('Asia/Kolkata').format('HH:mm:ss DD/MM/YYYY')}\n\n`
+                    text += ` *الرقم:* wa.me/${anu[0].jid.split("@")[0]}\n *البايو :* ${anu1.status}\n*التاريخ :* ${moment(anu1.setAt).tz('Asia/Kolkata').format('HH:mm:ss DD/MM/YYYY')}\n\n`
                 }
             } catch {
                 nowhatsapp += `${number0}${i}${number1}\n`
